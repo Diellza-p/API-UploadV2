2c88710e9daade9d10683cac5bc15e919bd64fe3
Dren <drenbilalli9@gmail.com>
2025-11-08 20:25:41 +0100

    d

--- controllers/content_controller.go
+++ controllers/content_controller.go
@@ -29,7 +29,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"gorm.io/gorm/logger"
 )
 
 func getContentCollection() *mongo.Collection {
